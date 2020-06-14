9401b4122d55a92265c486a6cb16e72d3aec139d
ameCont <americanCont7@gmail.com>
2020-06-14 10:20:47 -0400

    part2 final

---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'vue',
+    user: process.env.DB_USER || 'vue',
+    password: process.env.DB_PASS || 'vue',
+    options: {
+      dialect: process.env.DIALECT || 'sqlite',
+      host: process.env.HOST || 'localhost',
+      storage: './vue.sqlite'
+    }
+  }
+}
